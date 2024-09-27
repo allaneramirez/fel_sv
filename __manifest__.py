5d1d057984d7796dd79c22a4613ee8d52f72935d
Rodrigo Fernandez <rodrigo@aquih.com>
2024-09-27 12:30:48 -0600

    Version 13

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,6 @@
         'views/account_view.xml',
         'views/res_company_view.xml',
         'views/partner_view.xml',
-        'views/report_invoice.xml',
     ],
     'demo': [],
     'installable': True
