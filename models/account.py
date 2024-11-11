c5c5302b7486e89b311436674abc84ed1c4e67be
Rodrigo Fernandez <rodrigo@aquih.com>
2024-11-11 10:54:04 -0600

    Revert "Merge branch 'main' into 13.0"
    This reverts commit 381ede791a7675cbd81fd06891b96d95557e4cf3, reversing
    changes made to eb2987db2dbd148da8165886e8c9d8357363444a.

--- models/account.py
+++ models/account.py
@@ -2,34 +2,18 @@
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
-from odoo.release import version_info
-
-from lxml import etree
-from datetime import datetime
-
-import base64
-import re
-import json
-
-import odoo.addons.l10n_gt_extra.a_letras as a_letras
-
-#from OpenSSL import crypto
-#import xmlsig
-#from xades import XAdESContext, template, utils, ObjectIdentifier
-#from xades.policy import GenericPolicyId, ImpliedPolicy
 
 import logging
-import re
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
     firma_fel_sv = fields.Char('Firma FEL SV', copy=False)
     condicion_pago_fel_sv = fields.Selection([('1', 'Contado'), ('2', 'Crédito'), ('3', 'Otro')], 'Condicion de Pago FEL SV')
     forma_pago_fel_sv = fields.Selection([('01', 'Billetes y monedas'), ('02', 'Tarjeta Débito'), ('03', 'Tarjeta Crédito')], 'Forma de Pago FEL SV')
     motivo_fel_sv = fields.Char(string='Motivo FEL SV')
     tipo_anulacion_fel_sv = fields.Char(string='Tipo de Anulación FEL SV')
-    factura_nueva_fel_sv_id = fields.Many2one('account.invoice', string="Factura Nueva FEL SV")
+    factura_nueva_fel_sv_id = fields.Many2one('account.move', string="Factura Nueva FEL SV")
     factura_original_fel_sv_id = fields.Many2one('account.move', string="Factura Original FEL SV")
     responsable_fel_sv_id = fields.Many2one('res.partner', string="Responsable FEL SV")
     solicitante_fel_sv_id = fields.Many2one('res.partner', string="Solicitante FEL SV")
@@ -71,7 +55,7 @@ class AccountJournal(models.Model):
     _inherit = "account.journal"
 
     generar_fel_sv = fields.Boolean('Generar FEL SV')
-    tipo_documento_fel_sv = fields.Selection([('1', 'Factura'), ('3', 'Comprobante de crédito fiscal'), ('4', 'Nota de remisión'), ('5', 'Nota de crédito'), ('6', 'Nota de débito'), ('7', 'Comprobante de retención'), ('8', 'Comprobante de liquidación'), ('9', 'Documento contable de liquidación'), ('11', 'Facturas de exportación'), ('14', 'Factura de sujeto excluido'), ('15', 'Comprobante de donación')], 'Tipo de Documento FEL', copy=False)
+    tipo_documento_fel_sv = fields.Selection([('1', 'Factura'), ('3', 'Comprobante de crédito fiscal'), ('4', 'Nota de remisión'), ('5', 'Nota de crédito'), ('6', 'Nota de débito'), ('7', 'Comprobante de retención'), ('8', 'Comprobante de liquidación'), ('9', 'Documento contable de liquidación'), ('11', 'Facturas de exportación'), ('14', 'Factura de sujeto excluido'), ('15', 'Comprobante de donación')], 'Tipo de Documento FEL SV', copy=False)
     condicion_pago_fel_sv = fields.Selection([('1', 'Contado'), ('2', 'Crédito'), ('3', 'Otro')], 'Condicion de Pago por Defecto FEL SV')
     forma_pago_fel_sv = fields.Selection([('01', 'Billetes y monedas'), ('02', 'Tarjeta Débito'), ('03', 'Tarjeta Crédito')], 'Forma de Pago por Defecto FEL SV')
     codigo_establecimiento_sv = fields.Char('Código Establecimiento FEL SV')
